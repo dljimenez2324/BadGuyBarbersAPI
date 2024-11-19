69d1e9a8c7f89147aefd6e6e0df5355a0a38a937
dljimenez2324 <dljimenez2324@gmail.com>
2024-11-18 22:36:50 -0800

    added verifyuserpassword and hashpassword services

--- API/Services/UserService.cs
+++ API/Services/UserService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using API.Context;
 using API.Models;
@@ -31,9 +32,30 @@ namespace API.Services
 
             return result;
         }
-        // public PasswordDTO HashPassword(string password)
-        // {
+        public PasswordDTO HashPassword(string password)
+        {
+            // new instance of our passworddto
+            PasswordDTO newHashedPassword = new PasswordDTO();
 
-        // }
+            byte[] SaltBytes = new byte[64];  // create a new instance of eof byte 64 array and save to Saltbytes
+            var provider = new RNGCryptoServiceProvider();   // random number generator for cryptography
+            provider.GetNonZeroBytes(SaltBytes);  //removing zeros from SaltBytes
+            var Salt = Convert.ToBase64String(SaltBytes);  // create a variable for our Salt and will encrypt SaltBytes
+            var Rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, SaltBytes, 10000);  // Start of Hash created here with args password as the input from our input, SaltBytes which is our salt, and # of iterations
+            var Hash = Convert.ToBase64String(Rfc2898DeriveBytes.GetBytes(256));  //  encrypted string
+
+            newHashedPassword.Salt = Salt;
+            newHashedPassword.Hash = Hash;
+            return newHashedPassword;
+        }
+        
+        // function to veryify the users password
+        public bool VerifyUserPassword(string? Password, string? StoredHash, string? StoredSalt) 
+        {
+            var SaltBytes = Convert.FromBase64String(StoredSalt);
+            var rfc2898DeriveBytes = new Rfc2898DeriveBytes(Password, SaltBytes, 10000);
+            var newHash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
+            return newHash == StoredHash;
+        }
     }
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3e3247b2b9b5f088d5ee8131947b4990f434c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+455e5744190e5ec57999c3e7713fb9e75cc882e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
