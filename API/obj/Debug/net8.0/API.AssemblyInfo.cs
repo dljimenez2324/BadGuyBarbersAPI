6a30dfd2cb7750bc29ece02bb4951999119c26f9
dljimenez2324 <dljimenez2324@gmail.com>
2024-12-02 21:38:57 -0800

    changes made after making web app service

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52185bf22e9f77e57088af1cadaf9b2d3e6cccd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad87707f4762ad5aeaf6a78e5440c6ff42402095")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
