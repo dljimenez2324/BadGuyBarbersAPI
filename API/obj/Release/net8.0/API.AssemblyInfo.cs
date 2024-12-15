04c47498b921017232d25c7de3fd951a9290def7
dljimenez2324 <dljimenez2324@gmail.com>
2024-12-14 22:38:08 -0800

    redeployed the web ap and so some files are being deleted and remade

--- API/obj/Release/net8.0/API.AssemblyInfo.cs
+++ API/obj/Release/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edec4f950b52684e91f9abfd6f0d49efd91c85ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db71be2170b94dbe00f227f17edb9579424272a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
