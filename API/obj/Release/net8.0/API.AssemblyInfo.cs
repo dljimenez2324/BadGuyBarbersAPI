970f205f0bd2853cf8f8a5afe20f7937c0d593f9
dljimenez2324 <dljimenez2324@gmail.com>
2024-12-15 20:11:04 -0800

    full deployment to the Asure Webapp required these file changes.

--- API/obj/Release/net8.0/API.AssemblyInfo.cs
+++ API/obj/Release/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db71be2170b94dbe00f227f17edb9579424272a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4cb2ee808da1e16a33f4ac5d774b1cf70ce449")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
